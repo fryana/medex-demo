874beed6f42fd7bf8c845d6245242a9edb58c63f
Ryana <42619684+fryana@users.noreply.github.com>
2025-02-15 17:18:41 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -111,7 +111,8 @@ def generate_claude_report(image_base64):
 
 def generate_gpt_report(temp_path):
     # Stream OpenAI GPT-4o Vision response
-    client = openai(organization=GPT_ORG_KEY, project=GPT_API_KEY)
+    client = openai.OpenAI(api_key=GPT_API_KEY, organization=GPT_ORG_KEY)
+    print(client)
     
     stream = client.chat.completions.create(
         model="gpt-4o",
